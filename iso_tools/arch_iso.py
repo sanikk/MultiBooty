367783f7b960062c2dd50a9b859eef7a2e6b1cac
Samuli Nikkilä <sanikk@users.noreply.github.com>
2025-02-24 14:16:05 +0200

    working version of archiso
    removed one context manager

--- iso_tools/arch_iso.py
+++ iso_tools/arch_iso.py
@@ -2,34 +2,32 @@ import pycdlib
 from PySquashfsImage import SquashFsImage
 
 
-def list_packages_from_iso(iso_path):
-    with pycdlib.PyCdlib() as iso:
-        iso.open(iso_path)
+def list_packages_from_arch_iso(iso_path):
+    # with pycdlib.PyCdlib() as iso:
+    iso = pycdlib.PyCdlib()
+    iso.open(iso_path)
 
-        sfs_path = None
-        for child in iso.list_children(iso_path="/ARCH/X86_64"):
-            file_id = child.file_identifier().decode("utf-8")
-            if file_id.startswith("AIROOTFS.SFS"):
-                sfs_path = f"/ARCH/X86_64/{file_id}"
-                break
+    sfs_path = None
+    for child in iso.list_children(iso_path="/ARCH/X86_64"):
+        file_id = child.file_identifier().decode("utf-8")
+        if file_id.startswith("AIROOTFS.SFS"):
+            sfs_path = f"/ARCH/X86_64/{file_id}"
+            break
 
-        if not sfs_path:
-            raise FileNotFoundError("AIROOTFS.SFS not found in the ISO.")
+    if not sfs_path:
+        raise FileNotFoundError("AIROOTFS.SFS not found in the ISO.")
 
-        sfs_data = bytearray()
-        with iso.open_file_from_iso(iso_path=sfs_path) as f:
-            sfs_data.extend(f.read())
-
+    sfs_data = bytearray()
+    with iso.open_file_from_iso(iso_path=sfs_path) as f:
+        sfs_data.extend(f.read())
+    iso.close()
     with SquashFsImage.from_bytes(bytes(sfs_data)) as sfs_image:
         pacman_local_dir = sfs_image.select("/var/lib/pacman/local")
-        packages = [
-            entry.name for entry in pacman_local_dir.iterdir() if entry.is_dir()
-        ]
-
+        packages = [entry.name for entry in pacman_local_dir.iterdir() if entry.is_dir]
     return packages
 
 
 if __name__ == "__main__":
     iso_path = "archlinux-2024.11.01-x86_64.iso"
-    packages = list_packages_from_iso(iso_path)
+    packages = list_packages_from_arch_iso(iso_path)
     print(packages)
