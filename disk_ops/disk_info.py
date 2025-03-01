e5964f942a887333f4a0636b9612f4943e982954
Samuli Nikkilä <sanikk@users.noreply.github.com>
2025-03-01 02:44:44 +0200

    disk_ops/disk_info: returns dict on fail, couple of todos, better messages

--- disk_ops/disk_info.py
+++ disk_ops/disk_info.py
@@ -2,8 +2,12 @@ from disk_ops.runners import run_subprocess_with_sudo
 import json
 
 
-def get_disk_info(devs):
+def get_disk_info(devs: list[str]) -> dict:
+    # TODO: move this printing to it's own function that we inject here if using text UI.
+    # TODO: make one like this for GUI too, and inject it here if using GUI.
     print("We need sudo to collect device info.")
+    print("We'll be reading sector size and size in bytes")
+    print(f"from devices: {devs}.")
     print("This is a read-only op.")
     ret = run_subprocess_with_sudo(
         "disk_ops/disk_info_linux.py", devs, FileNotFoundError, "No such device."
@@ -16,4 +20,4 @@ def get_disk_info(devs):
             print("Error: no return value from gathering device info")
         else:
             print("Error:", ret.stderr)
-        return None
+        return {}
