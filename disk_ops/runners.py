5bee6cecdb76a720df83862b83d9b06860e79ce9
Samuli Nikkilä <sanikk@users.noreply.github.com>
2025-03-29 12:14:24 +0200

    disk_ops reorg part 1
    made dirs under disk_ops:
    disks/
    partitions/
    
    moved the makefs commands to disk_ops/make_filesystems.py

--- disk_ops/runners.py
+++ disk_ops/runners.py
@@ -82,23 +82,3 @@ def make_partitions(
         # if ret.returncode == 0:
 
     return ""
-
-
-def make_boot_filesystem(dev, partition):
-    """
-    Make FAT32 filesystem.
-    """
-    run_subprocess_with_sudo(
-        "mkfs.fat", ["-F32", f"{dev}{partition}"], ValueError, "bleb"
-    )
-    # subprocess.run(["mkfs.fat", "-F32", f"{dev}{partition}"], check=True)
-
-
-def make_ext_filesystem(dev, partition):
-    """
-    Make ext4 filesystem with no journaling.
-    """
-    run_subprocess_with_sudo(
-        "mkfs.ext4", ["-O", "^has_journal", f"{dev}{partition}"], ValueError, "blob"
-    )
-    # subprocess.run(["mkfs.ext4", "-O", "^has_journal", f"{dev}{partition}"], check=True)
