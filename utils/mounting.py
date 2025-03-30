ef160bfb8ab3256d859f6d44161c45e83ffe4f96
Samuli Nikkilä <sanikk@users.noreply.github.com>
2025-03-30 21:01:33 +0300

    utils: moved mounting and runners to utils. enchanced functionality in mounting now it works better

---
+++ utils/mounting.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+from utils.runners import run_subprocess_with_sudo
+
+from subprocess import run
+
+
+def is_mounted(mountpoint):
+    with open("/proc/mounts") as f:
+        return any(line.split()[1] == mountpoint for line in f)
+
+
+def mount_command(dev, mountpoint):
+    run_subprocess_with_sudo("mount", [dev, mountpoint], ValueError, "mount")
+
+
+def unmount_command(dev):
+    run_subprocess_with_sudo("umount", [dev], ValueError, "umount")
+
+
+def mounted(func):
+    def wrapper(partition, mountpoint, *args, **kwargs):
+        if is_mounted(mountpoint):
+            return False, f"{mountpoint} was not empty."
+        mount_command(partition, mountpoint)
+        try:
+            if not is_mounted(mountpoint):
+                return False, f"Mounting {partition} on {mountpoint} failed."
+            return func(partition, mountpoint, *args, **kwargs)
+        finally:
+            unmount_command(partition)
+
+    return wrapper
